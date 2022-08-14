845b3755bee6093833c1eea19236d375bf948938
chriserl <chriserldev@gmail.com>
2022-08-14 08:30:09 +0000

    feature: exported types from navigation in index

--- src/library/index.ts
+++ src/library/index.ts
@@ -4,3 +4,8 @@ export { respx } from "./utils/stitches/respx";
 export { ArcaneLink } from "./atoms/ArcaneLink/ArcaneLink";
 export { Button } from "./atoms/Button/Button";
 export { Navigation } from "./widgets/Navigation/Navigation";
+
+export type {
+	NavLinksDataShape,
+	NavigationDataShape,
+} from "./widgets/Navigation/Navigation";
