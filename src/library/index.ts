6492bcdd206d934824f8f3ab34b2fe48118671c2
chriserl <chriserldev@gmail.com>
2023-05-03 06:14:18 +0000

    update: exported new form components and types from index file

--- src/library/index.ts
+++ src/library/index.ts
@@ -3,12 +3,18 @@ export { arcaneBase } from "./utils/arcaneBase";
 export { respx } from "./utils/respx";
 export { Anchor } from "./elements/Anchor/Anchor";
 export { Button } from "./elements/Button/Button";
+export { Input } from "./elements/Forms/Input";
+export { TextArea } from "./elements/Forms/TextArea";
+export { InputWrapper } from "./elements/Forms/InputWrapper";
 export { NavigationWidget } from "./widgets/NavigationWidget/NavigationWidget";
 export { CarouselContainer } from "./containers/CarouselContainer";
 export { FooterWidget } from "./widgets/FooterWidget/FooterWidget";
 
 export type { AnchorShape } from "./elements/Anchor/Anchor";
 export type { ButtonShape } from "./elements/Button/Button";
+export type { InputShape } from "./elements/Forms/Input";
+export type { TextAreaShape } from "./elements/Forms/TextArea";
+export type { InputWrapperShape } from "./elements/Forms/InputWrapper";
 export type {
 	NavLinkFragmentDataShape,
 	NavigationWidgetDataShape,
